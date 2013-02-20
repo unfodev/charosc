1f5b435297cf5ff250bb0c4a301728c1c2a70c49
Dan Richert <dan.richert@gmail.com>
2013-02-19 20:17:14 -0500

    remove some junk

--- lib/charosc/oscillator.rb
+++ lib/charosc/oscillator.rb
@@ -1,5 +1,5 @@
 module Charosc
-  class Oscillator
+  class Generator
     attr_reader :words
 
     def initialize(str)
@@ -18,22 +18,3 @@ module Charosc
     end
   end
 end
-
-
-class Array
-  def indexes_of(obj)
-    [].tap do |out|
-      self.each_with_index do |o, ndx|
-        out << ndx if o == obj
-      end
-    end
-
-    #inject([]) do |acc, el|
-    #  acc << find_index(el) if el == obj
-    #  acc
-    #end
-  end
-end
-
-
-p [1, 2, 3, 4, 6, 6, 7, 8, 5, 6, 4, 3,3 ].indexes_of(5)
